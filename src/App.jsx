0d70492a7470f4a2b6b9e9659a830a8fa2b97ffc
Hidash <hidash920@gmail.com>
2025-12-12 15:45:43 +0530

    something

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Hidash</h1>
+      <h1>Something + Hidash</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
